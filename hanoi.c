80f0181922e26c4f193315c5bebdcd5de4edf3e5
TeisRaul <teisteisraul@gmail.com>
2023-10-10 17:20:09 +0300

    Hanoi mutari si 2 conditii

--- hanoi.c
+++ hanoi.c
@@ -4,10 +4,25 @@
 
 void hanoi(int n, int a, int b, int c)
 {
-       
+   if(n<1)
+      printf("Nu exista discuri");
+    else
+        if(n == 1)
+        printf("Se excuta o mutare de pe tija A pe tija C");
+}
+
+void mutari(int n)
+{
+    int i, putere=1;
+    for(i=0;i<n;i++)
+       putere *=2;
+    printf("Numarul de mutari minim este %d", putere);
 }
 
 int main()
 {
-
+    int n,a,b,c,i;
+    printf("Introduceti numarul de discuri: ");
+    scanf("%d", &n);
+    mutari(&n);
 }
